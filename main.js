a44490618956932dd567db644c27ca2072e60a5e
Bruno DeLucca <bruno@delucca.cl>
2020-07-14 18:02:52 +0200

    be able to create a new task with all the Elements inside.

--- main.js
+++ main.js
@@ -1,14 +1,66 @@
+// Make all the parts of the taskItem
 
-
-function addTask() {
-    const text = document.getElementById('inputField').value;
-
-    if (text) {
-        const newElement = document.createElement('div');
-        newElement.innerText = text;
-        newElement.classList.add('taskItem');
-        document.getElementById('taskLists').append(newElement);
-        document.getElementById('inputField').value = '';
-    }
-
+function makeACheckBox() {
+  const newCheckbox = document.createElement("input");
+  newCheckbox.type = "checkbox";
+  newCheckbox.className = "taskCheckbox";
+  return newCheckbox;
 }
+
+function makeAEdit() {
+  const newEdit = document.createElement("div");
+  newEdit.innerHTML = "Edit";
+  newEdit.className = "editButton";
+  return newEdit;
+}
+
+function makeADelete() {
+  const newDelete = document.createElement("div");
+  newDelete.innerHTML = "Delete";
+  newDelete.className = "deleteButton";
+  return newDelete;
+}
+
+function makeAText(text) {
+  const newText = document.createElement("div");
+  newText.innerText = text;
+  newText.classList.add("taskItem");
+  return newText;
+}
+
+// Make the taskItem
+function makeATaskItem(text) {
+  const task = document.createElement("div");
+  task.id = "taskItem";
+  const chechBox = makeACheckBox();
+  const edited = makeAEdit();
+  const deleted = makeADelete();
+  const textbox = makeAText(text);
+  taskItem.appendChild(chechBox);
+  taskItem.appendChild(textbox);
+  taskItem.appendChild(edited);
+  taskItem.appendChild(deleted);
+  return task;
+}
+
+// Function addTask()
+function addTask() {
+  const text = document.getElementById("inputField").value;
+  const taskContainer = document.getElementById("taskLists");
+  if (text) {
+    const taskItem = makeATaskItem(text);
+    taskContainer.append(taskItem);
+    text.value = "";
+  }
+}
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  addTask();
+};
+
+inputBox.onkeydown = function (e) {
+  if (e.keyCode === 13) {
+    addTask();
+  }
+};
